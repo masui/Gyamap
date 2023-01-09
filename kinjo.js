3c7f48baaf3c82cd4725dab46a2be0f0516c8048
Toshiyuki Masui <masui@pitecan.com>
2023-01-09 20:13:36 +0900

    ...

---
+++ kinjo.js
@@ -0,0 +1,41 @@
+function loadScript (url) {
+    return new Promise((resolve) => {
+        const script = document.createElement('script')
+        script.src = url
+        script.onload = resolve
+        document.body.appendChild(script)
+    })
+}
+async function loadAll() {
+    await loadScript('https://scrapbox.io/api/code/masui/test/test.js')
+    // alert(data)
+}
+
+$(function(){
+    loadAll()
+})
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+function successCallback(position) {
+    mapsurl = "https://maps.google.com/maps?q=" +
+        position.coords.latitude + "," +
+        position.coords.longitude;
+    location.href = mapsurl;
+}
+function errorCallback(error) {
+    var err_msg = "";
+    switch(error.code)
+    {
+        case 1:
+        err_msg = "位置情報の利用が許可されていません";
+        break;
+        case 2:
+        err_msg = "デバイスの位置が判定できません";
+        break;
+        case 3:
+        err_msg = "タイムアウトしました";
+        break;
+    }
+    alert(err_msg)
+    //document.getElementById("show_result").innerHTML = err_msg;
+}
