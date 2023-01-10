24129aca6523ff9a0cb85d38db239acfcb2c09ae
Toshiyuki Masui <masui@pitecan.com>
2023-01-10 13:08:14 +0900

    ...

--- kinjo.js
+++ kinjo.js
@@ -30,12 +30,17 @@ function distance(lat1, lng1, lat2, lng2) {
     return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
 }
 
+var locations = []
 for(let i=0; i<data.length; i++){
     let m = data[i].match(/\[\/(.*)\/(.*)\]\s+.*@([\d\.]+),([\d\.]+)/)
     entry = {}
-    entry.project = 'xxxx'
-    console.log(m)
+    entry.project = m[1]
+    entry.title = m[2]
+    entry.latitude = m[3]
+    entry.longitude = m[4]
+    locations.push[entry]
 }
+alert(locations.length)
 
 navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 function successCallback(position) {
