355a0345f00d4921851946840b8794245d2818d3
Toshiyuki Masui <masui@pitecan.com>
2023-01-10 12:35:53 +0900

    ...

--- kinjo.js
+++ kinjo.js
@@ -18,11 +18,22 @@ async function loadAll() {
 $(function(){
     //loadAll()
     // setTimeout(function(){ alert(data[2]) }, 5000)
-    alert(data[2])
+    // alert(data[2])
 })
+
+function distance(lat1, lng1, lat2, lng2) {
+    const R = Math.PI / 180;
+    lat1 *= R;
+    lng1 *= R;
+    lat2 *= R;
+    lng2 *= R;
+    return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
+}
 
 for(let i=0; i<data.length; i++){
     let m = data[i].match(/\[\/(.*)\/(.*)\]\s+.*@([\d\.]+),([\d\.]+)/)
+    entry = {}
+    entry.project = 'xxxx'
     alert(m)
 }
 
