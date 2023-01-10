e35d6d4692b21ac466962c761678246a6014b428
Toshiyuki Masui <masui@pitecan.com>
2023-01-10 10:35:57 +0900

    ...

--- kinjo.js
+++ kinjo.js
@@ -13,11 +13,11 @@ function loadScript (url) {
 }
 async function loadAll() {
     await loadScript('https://scrapbox.io/api/code/masui/POI/poi.js')
+    alert(data[2])
 }
 
 $(function(){
     loadAll()
-    alert(data[2])
 })
 
 navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
