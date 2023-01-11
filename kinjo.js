2c4f830943ffc7e9006bc41a29b607116517ebf0
Toshiyuki Masui <masui@pitecan.com>
2023-01-11 11:04:29 +0900

    ...

--- kinjo.js
+++ kinjo.js
@@ -36,8 +36,8 @@ function distance(lat1, lng1, lat2, lng2) {
     return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
 }
 
+var locations = []
 function setlocations(){
-    var locations = []
     for(let i=0; i<data.length; i++){
 	let m = data[i].match(/\[\/(.*)\/(.*)\]\s+.*@([\d\.]+),([\d\.]+),(\d+)z/)
 	entry = {}
